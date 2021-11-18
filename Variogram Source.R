2efcbf42e5e6ef840353f96025ba514cf37b8575
Lauren Temple <89480531+ltemps@users.noreply.github.com>
2021-11-18 17:25:43 -0500

    data set of all buoys figured out

--- Variogram Source.R
+++ Variogram Source.R
@@ -12,16 +12,14 @@ W <- c(76.500, 93.646, 85.615, 95.345, 90.900, 93.100, 80.376, 81.109)
 location_data <- data.frame(Buoy, N, W)
 
 #combine YY MM DD
-st41016$Date <- str_c(st41016$YY, st41016$MM, st41016$DD)
-st42002$Date <- str_c(st42002$YY, st42002$MM, st42002$DD)
-st42003$Date <- str_c(st42003$YY, st42003$MM, st42003$DD)
-st42019$Date <- str_c(st42019$YY, st42019$MM, st42019$DD)
-stbusl1$Date <- str_c(stbusl1$YY, stbusl1$MM, stbusl1$DD)
-stgbcl1$Date <- str_c(stgbcl1$YY, stgbcl1$MM, stgbcl1$DD)
-stmlrf1$Date <- str_c(stmlrf1$YY, stmlrf1$MM, stmlrf1$DD)
-stsmkf1$Date <- str_c(stsmkf1$YY, stsmkf1$MM, stsmkf1$DD)
-st
-#lubridate package
+st41016$Date <- format(as.Date(with(st41016, paste(YY, MM, DD,sep="-")), "%Y-%m-%d"), "19%y-%m-%d")
+st42002$Date <- format(as.Date(with(st42002, paste(YY, MM, DD,sep="-")), "%Y-%m-%d"), "19%y-%m-%d")
+st42003$Date <- format(as.Date(with(st42003, paste(YY, MM, DD,sep="-")), "%Y-%m-%d"), "19%y-%m-%d")
+st42019$Date <- format(as.Date(with(st42019, paste(YY, MM, DD,sep="-")), "%Y-%m-%d"), "19%y-%m-%d")
+stbusl1$Date <- format(as.Date(with(stbusl1, paste(YY, MM, DD,sep="-")), "%Y-%m-%d"), "19%y-%m-%d")
+stgbcl1$Date <- format(as.Date(with(stgbcl1, paste(YY, MM, DD,sep="-")), "%Y-%m-%d"), "19%y-%m-%d")
+stmlrf1$Date <- format(as.Date(with(stmlrf1, paste(YY, MM, DD,sep="-")), "%Y-%m-%d"), "19%y-%m-%d")
+stsmkf1$Date <- format(as.Date(with(stsmkf1, paste(YY, MM, DD,sep="-")), "%Y-%m-%d"), "19%y-%m-%d")
 
 #filter out uneeded columns
 #probably a way to write a function for this?
@@ -33,16 +31,6 @@ bBUSL1 <- stbusl1[-c(1:5, 8:11, 13:16)]
 bGBCL1 <- stgbcl1[-c(1:5, 8:11, 13:16)]
 bMLRF1 <- stmlrf1[-c(1:5, 8:11, 13:16)]
 bSMKF1 <- stsmkf1[-c(1:5, 8:11, 13:16)]
-
-#add buoy name
-b41016$buoy <- "41016"
-b42002$buoy <- "42002"
-b42003$buoy <- "42003"
-b42019$buoy <- "42019"
-bBUSL1$buoy <- "BUSL1"
-bGBCL1$buoy <- "GBCL1"
-bMLRF1$buoy <- "MLRF1"
-bSMKF1$buoy <- "SMKF1"
 
 #turn this into a function that will run for each buoy
 meantables<- function(i){
@@ -59,15 +47,30 @@ mean_data <- left_join(mean_WSPD, mean_GST, by="Date")
 mean_data <- left_join(mean_data, mean_GST, by="Date")
 return(mean_data)
 }
-meantables(bSMKF1)
+#that works just have to do for each buoy
+b41016 <- meantables(b41016)
+b42002 <- meantables(b42002)
+b42003 <- meantables(b42003)
+b42019 <- meantables(b42019)
+bBUSL1 <- meantables(bBUSL1)
+bGBCL1 <- meantables(bGBCL1)
+bMLRF1 <- meantables(bMLRF1)
+bSMKF1 <- meantables(bSMKF1)
 
-#make a table of all the buoys
-#something along the lines of this
-vario_data <- left_join(mean_WSPD, mean_GST, by="Date")
-vario_data <- left_join(vario_data, mean_BAR, by="Date")
+#add buoy name to the mean tables
+b41016$Buoy <- "41016"
+b42002$Buoy <- "42002"
+b42003$Buoy <- "42003"
+b42019$Buoy <- "42019"
+bBUSL1$Buoy <- "BUSL1"
+bGBCL1$Buoy <- "GBCL1"
+bMLRF1$Buoy <- "MLRF1"
+bSMKF1$Buoy <- "SMKF1"
 
-#then add on location of each buoy
+#then join them together
+all_buoy <- rbind(b41016, b42002, b42003, b42019, bBUSL1, 
+                  bGBCL1, bMLRF1, bSMKF1)
+all_buoy <- left_join(all_buoy, location_data, by= "Buoy")
 
-#then I think I can make the variogram for each day
+#make a function for variogram that pulls any date you want 
 
-#in for loop add column for latitude and longitude that goes to the buoy 
